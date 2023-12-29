93fe37844ac59ba4fea088bc4e7d45a7ebaf632c
higorless <higor.fernandes98@hotmail.com>
2023-12-29 15:50:43 -0300

    Sidemenu finlizado e início de desenvolvimendo dos posts

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,8 +1,30 @@
-export function Post (props) {
+import styles from './Post.module.css'
+
+export function Post () {
   return (
-    <>
-      <p> {props.author}</p>
-      <p> {props.content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}> 
+          <img className={styles.avatar} src="https://github.com/higorless.png"></img>
+          <div className={styles.authorInfo}>
+            <strong>Higor Gomes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 8:13" dateTime="">Publicado </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala Galeraa</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Reutnr, evento da 
+        rocket seat</p>
+
+        <p> higor.desing/legal</p>
+
+        <p> #novoprojeto </p>
+      </div>
+    </article>
   )
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar () {
@@ -6,12 +8,15 @@ export function Sidebar () {
       <img className ={styles.cover} src="https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
 
       <div className={styles.profile}> 
+        <img src="https://github.com/higorless.png"></img> 
+
         <strong> Diego Fernandes </strong>
         <span> Web Developer </span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine/>
           Editar o seu perfil
         </a>
       </footer>
